dd77a3a7b61547a71ecf5d13f6e85f13a59e9805
Maitri Vira <maitrivira1@gmail.com>
2021-07-13 15:42:46 +0700

    structure file and main view

--- MediTime/Controller/ViewController.swift
+++ MediTime/Controller/ViewController.swift
@@ -11,9 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "MediTime"
+        navigationItem.largeTitleDisplayMode = .never
     }
 
-
+    
 }
 
